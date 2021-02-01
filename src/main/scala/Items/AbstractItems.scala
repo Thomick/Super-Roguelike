3c3f82129e9f6ac7a69548fd8a416163daca5e2c
Thomas MICHEL <tmdu13@gmail.com>
2021-02-01 14:48:22 +0100

    consumable and throwable traits

--- src/main/scala/Items/AbstractItems.scala
+++ src/main/scala/Items/AbstractItems.scala
@@ -6,6 +6,14 @@ import GameEntities._
 abstract class AbstractItem() {
   val name: String
   val description: String
+  val weight: Int
+}
+
+trait Consumable {
+  def consume: Boolean
+}
+
+trait Throwable {
   def throwItem(
       board: GameBoard,
       startingPos: (Int, Int),
