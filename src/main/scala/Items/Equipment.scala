10cb63cf1fd0b0739d6cae1ea8598f39657dc2a2
Thomas MICHEL <tmdu13@gmail.com>
2021-02-05 13:27:32 +0100

    add default value for armor and basic weapon

--- src/main/scala/Items/Equipment.scala
+++ src/main/scala/Items/Equipment.scala
@@ -1,7 +1,12 @@
 package Items
 
-abstract class Armor() extends AbstractItem with Equipable with Passive {}
+abstract class Armor() extends AbstractItem with Equipable with Passive {
+  val bonusDef: Int = 0
+  val bonusHP: Int = 0
+}
 
 abstract class BasicWeapon() extends AbstractItem with Equipable with Passive {
   val part = BodyPart.Hand
+  val bonusAtt: Int = 0
+  val bonusHP: Int = 0
 }
