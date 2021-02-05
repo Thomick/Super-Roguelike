afed0c42f039bcd038e28a1fde09a330533fe929
Thomas MICHEL <tmdu13@gmail.com>
2021-02-06 00:20:17 +0100

    Adapt board size to window size

--- src/main/scala/Rendering/BoardRenderer.scala
+++ src/main/scala/Rendering/BoardRenderer.scala
@@ -6,27 +6,29 @@ import map_objects._
 import GameEntities._
 import fov_functions._
 
-class BoardRenderer(boardSize: Int, nbTiles: Int) {
+object BoardRenderer {
   val lightfloorColor = new Color(99, 150, 150)
   val lightwallColor = new Color(168, 255, 255)
   val darkfloorColor = new Color(79, 119, 119)
   val darkwallColor = new Color(134, 204, 204)
   val errorColor = new Color(255, 0, 0)
-  val tileSize = boardSize / nbTiles
-  val tileOffset = 0
+  val tilePadding = 0
 
   def drawBoard(
       g: Graphics2D,
       gridOrigin: (Int, Int),
       board: GameBoard,
       fovmap: FovMap,
-      baseTile: (Int, Int)
+      baseTile: (Int, Int),
+      boardSize: Int,
+      nbTiles: Int
   ): Unit = {
+    val tileSize = boardSize / (nbTiles + tilePadding)
 
     def buildRect(pos: (Int, Int)): Rectangle =
       new Rectangle(
-        gridOrigin._1 + pos._1 * (tileSize + tileOffset),
-        gridOrigin._2 + pos._2 * (tileSize + tileOffset),
+        gridOrigin._1 + pos._1 * (tileSize + tilePadding),
+        gridOrigin._2 + pos._2 * (tileSize + tilePadding),
         tileSize,
         tileSize
       )
@@ -59,7 +61,6 @@ class BoardRenderer(boardSize: Int, nbTiles: Int) {
             }
           }
           g.fill(buildRect(pos))
-          println("draw")
         }
       }
     }
@@ -68,8 +69,8 @@ class BoardRenderer(boardSize: Int, nbTiles: Int) {
         val img = Toolkit.getDefaultToolkit().getImage(e.image)
         g.drawImage(
           img,
-          e.pos._1 * (tileOffset + tileSize) + gridOrigin._1,
-          e.pos._2 * (tileOffset + tileSize) + gridOrigin._2,
+          e.pos._1 * (tilePadding + tileSize) + gridOrigin._1,
+          e.pos._2 * (tilePadding + tileSize) + gridOrigin._2,
           tileSize,
           tileSize,
           null
--- src/main/scala/Rendering/MainRenderer.scala
+++ src/main/scala/Rendering/MainRenderer.scala
@@ -1,8 +1,9 @@
 package Rendering
 
 import swing._
-import java.awt.Graphics2D
+import java.awt.{Graphics2D, Dimension}
 import java.awt.Color
+import scala.math.{min, max}
 import map_objects._
 import fov_functions._
 
@@ -10,19 +11,38 @@ class Renderer {
   val bgColor = new Color(48, 99, 99)
   val errorColor = new Color(255, 0, 0)
   val gridOrigin = (10, 10)
-  val BoardRenderer = new BoardRenderer(600, 30)
+  val rightPanelWidth = 200
+  val bottomPanelHeight = 0
+  val padding = 20
 
   def onPaint(
       g: Graphics2D,
       board: GameBoard,
       lastkey: String,
-      screen_width: Int,
-      screen_height: Int,
+      screenSize: Dimension,
       fovmap: FovMap
   ) {
     fovmap.compute_fov(board.playerEntity.pos._1, board.playerEntity.pos._2)
     g setColor bgColor
-    g fillRect (0, 0, screen_width, screen_height)
+    g fillRect (0, 0, screenSize.width, screenSize.height)
+    val drawingAreaWidth = screenSize.width - 2 * padding
+    val drawingAreaHeight = screenSize.height - 2 * padding
+    val boardSize = max(
+      0,
+      min(
+        drawingAreaWidth - rightPanelWidth - padding,
+        drawingAreaHeight - bottomPanelHeight - padding
+      )
+    )
+    BoardRenderer.drawBoard(
+      g,
+      (padding, padding),
+      board,
+      fovmap,
+      (0, 0),
+      boardSize,
+      30
+    )
     val infos =
       "Last key pressed : " + lastkey + "\nImplemented command : \n" +
         " - Move with Arrows keys \n" + " - E to pick items up (You must face it) \n" +
@@ -31,8 +51,7 @@ class Renderer {
     StringRenderer.drawString(
       g,
       infos,
-      (640, 20)
+      (boardSize + 2 * padding, padding)
     )
-    BoardRenderer.drawBoard(g, (20, 20), board, fovmap, (0, 0))
   }
 }
