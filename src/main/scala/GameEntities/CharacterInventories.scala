f1091179f0cba649dab0ccdbe84f5e7420d8d3c1
Thomas MICHEL <tmdu13@gmail.com>
2021-02-26 23:32:06 +0100

    Finish GameEntities code cleaning

--- src/main/scala/GameEntities/CharacterInventories.scala
+++ src/main/scala/GameEntities/CharacterInventories.scala
@@ -9,15 +9,14 @@ trait HasInventory extends Character {
 
   def getInventoryItems(): Array[AbstractItem] = inventory.toArray
 
+  // Add an item to the inventory
   def obtainItem(item: AbstractItem) = {
     inventory += item
     writeLog(name + " obtains " + item.name)
   }
 
-  def destroyItem(item: AbstractItem) = {
-    inventory -= item
-  }
-
+  // pick up an item from the ground (if any)
+  // returns true if an item has been picked up, else false
   def pickUpItem(): Boolean = {
     board.pickUpItem(pos, 0) match {
       case Some(item) => {
@@ -28,6 +27,8 @@ trait HasInventory extends Character {
     }
   }
 
+  // Drop the item of index (itemSlot) on the ground (if possible)
+  // returns true if the item has been dropped
   def dropItem(itemSlot: Int): Boolean = {
     if (!isSlotEmpty(itemSlot)) {
       val item = inventory(itemSlot)
@@ -39,6 +40,7 @@ trait HasInventory extends Character {
     return false
   }
 
+  // Test if an itemSlot is empty (ie. index out of bounds)
   def isSlotEmpty(itemSlot: Int, container: Seq[AbstractItem] = inventory): Boolean = {
     if (itemSlot < container.length)
       return false
@@ -46,6 +48,7 @@ trait HasInventory extends Character {
     return true
   }
 
+  // Throw an item if the item is throwable, the effect depends on the item
   def throwItem(itemSlot: Int, dir: Direction.Value): Boolean = {
     if (!isSlotEmpty(itemSlot)) {
       val item = inventory(itemSlot)
@@ -61,6 +64,7 @@ trait HasInventory extends Character {
     return false
   }
 
+  // Consume an item if the item is consumable, effect depends on the item
   def consumeItem(itemSlot: Int): Boolean = {
     if (!isSlotEmpty(itemSlot)) {
       val item = inventory(itemSlot)
@@ -76,18 +80,17 @@ trait HasInventory extends Character {
   }
 }
 
+// This trait allows the character to equip an equipable item and apply the stats bonus
+// Items can only be equiped if the body part on which they must be equiped is free
 trait CanEquip extends Character with HasInventory {
-
   val equipedItems = mutable.ArrayBuffer[Equipable]()
 
   def getEquipedItems(): Array[AbstractItem] = equipedItems.toArray
 
   override def getDef(): Int =
     baseDef + equipedItems.foldLeft[Int](0)((s, item) => s + item.bonusDef)
-
   override def getAtt(): Int =
     baseAtt + equipedItems.foldLeft[Int](0)((s, item) => s + item.bonusAtt)
-
   override def getMaxHP(): Int =
     baseMaxHP + equipedItems.foldLeft[Int](0)((s, item) => s + item.bonusHP)
 
@@ -124,6 +127,7 @@ trait CanEquip extends Character with HasInventory {
   def isBodyPartFree(part: BodyPart.Value): Boolean
 }
 
+// Trait describing a body type, implements isBodyPartFree
 trait Humanoid extends CanEquip {
   def isBodyPartFree(part: BodyPart.Value): Boolean = {
     val samePartCount = equipedItems.foldLeft[Int](0)((c, item) =>
