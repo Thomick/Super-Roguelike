99a05a9a099dad3de723306bcc56cffc930cd546
Thomas MICHEL <tmdu13@gmail.com>
2021-03-16 10:14:22 +0100

    Define status

---
+++ src/main/scala/game_entities/Status.scala
@@ -0,0 +1,53 @@
+package game_entities
+
+class StatusResults {
+  var canMove: Boolean = true
+  var healthModifier: Int = 0
+}
+
+abstract class Status(var remainingTime: Int) {
+  val name: String
+  val effectDescription: String
+
+  // Get remaining time as a string
+  def remainingTimeString(): String =
+    if (remainingTime < 0)
+      return "Forever"
+    else
+      return remainingTime.toString()
+
+  def applyEffect(result: StatusResults): Unit = {
+    if (remainingTime >= 0)
+      remainingTime -= 1
+  }
+}
+
+class BleedingStatus(remainingTime: Int) extends Status(remainingTime) {
+  val name = "Bleeding"
+  val effectDescription = "You lose health each turn"
+
+  override def applyEffect(result: StatusResults): Unit = {
+    result.healthModifier -= 3
+    super.applyEffect(result)
+  }
+}
+
+class RegenerationStatus(remainingTime: Int) extends Status(remainingTime) {
+  val name = "Regeneration"
+  val effectDescription = "You gain health each turn"
+
+  override def applyEffect(result: StatusResults): Unit = {
+    result.healthModifier += 5
+    super.applyEffect(result)
+  }
+}
+
+class StunnedStatus(remainingTime: Int) extends Status(remainingTime) {
+  val name = "Stunned"
+  val effectDescription = "You can move for the duration of the status"
+
+  override def applyEffect(result: StatusResults): Unit = {
+    result.canMove = false
+    super.applyEffect(result)
+  }
+}
