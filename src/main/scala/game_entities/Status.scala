a2ef55ed31182968b087ee032e4cc08a1dd57844
Thomas MICHEL <tmdu13@gmail.com>
2021-03-16 10:29:57 +0100

    Add status handler

--- src/main/scala/game_entities/Status.scala
+++ src/main/scala/game_entities/Status.scala
@@ -1,5 +1,14 @@
 package game_entities
 
+object StatusHandler {
+  // Gather the effects of the status in a StatusResults and return it.
+  def getEffects(statusCollection: Iterable[Status]): StatusResults = {
+    val results = new StatusResults
+    statusCollection.foreach(status => status.applyEffect(results))
+    return results
+  }
+}
+
 class StatusResults {
   var canMove: Boolean = true
   var healthModifier: Int = 0
@@ -16,7 +25,7 @@ abstract class Status(var remainingTime: Int) {
     else
       return remainingTime.toString()
 
-  def applyEffect(result: StatusResults): Unit = {
+  def applyEffect(results: StatusResults): Unit = {
     if (remainingTime >= 0)
       remainingTime -= 1
   }
@@ -26,9 +35,9 @@ class BleedingStatus(remainingTime: Int) extends Status(remainingTime) {
   val name = "Bleeding"
   val effectDescription = "You lose health each turn"
 
-  override def applyEffect(result: StatusResults): Unit = {
-    result.healthModifier -= 3
-    super.applyEffect(result)
+  override def applyEffect(results: StatusResults): Unit = {
+    results.healthModifier -= 3
+    super.applyEffect(results)
   }
 }
 
@@ -36,9 +45,9 @@ class RegenerationStatus(remainingTime: Int) extends Status(remainingTime) {
   val name = "Regeneration"
   val effectDescription = "You gain health each turn"
 
-  override def applyEffect(result: StatusResults): Unit = {
-    result.healthModifier += 5
-    super.applyEffect(result)
+  override def applyEffect(results: StatusResults): Unit = {
+    results.healthModifier += 5
+    super.applyEffect(results)
   }
 }
 
@@ -46,8 +55,8 @@ class StunnedStatus(remainingTime: Int) extends Status(remainingTime) {
   val name = "Stunned"
   val effectDescription = "You can move for the duration of the status"
 
-  override def applyEffect(result: StatusResults): Unit = {
-    result.canMove = false
-    super.applyEffect(result)
+  override def applyEffect(results: StatusResults): Unit = {
+    results.canMove = false
+    super.applyEffect(results)
   }
 }
