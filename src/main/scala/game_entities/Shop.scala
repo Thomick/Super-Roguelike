c1661e6b11f7a1fea8554cd02ac2dedd359404e6
Thomas MICHEL <tmdu13@gmail.com>
2021-03-19 17:31:35 +0100

    Add shopkeeper

--- src/main/scala/game_entities/Shop.scala
+++ src/main/scala/game_entities/Shop.scala
@@ -1,2 +1,10 @@
 package game_entities
 
+import map_objects._
+
+class Shopkeeper(init_pos: (Int, Int), b: GameBoard) extends Character(init_pos, b) with AIControlled {
+  val name = "Shopkeeper"
+  val description = "He sells low quality goods at a high price"
+
+  def action(c: Character) = ()
+}
