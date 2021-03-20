d46041b38b7d0d0f830e36c925752f66d0f8a132
Léo Mangel <leomangel@protonmail.com>
2021-03-20 17:40:42 +0100

    Use correct functions to round numbers

--- src/main/scala/Cursor.scala
+++ src/main/scala/Cursor.scala
@@ -2,6 +2,7 @@ package cursor
 
 import game_entities._
 import map_objects._
+import scala.math.{min, max}
 
 class Cursor (board : GameBoard) {
   val image = "src/main/resources/cursor.png"
@@ -45,6 +46,31 @@ class Cursor (board : GameBoard) {
       ypos = newpos._2
     }
   }
-  def highlightedCells : ... = {
+  def highlightedCells : Vector[(Int,Int)] = {
+    var hCells = Vector[(Int,Int)]()
+    val dx : Double = board.playerEntity.pos._1 - xpos
+    val dy : Double = board.playerEntity.pos._2 - ypos
+    var steps : Double = max(Math.abs(dx),Math.abs(dy))
+    val xIncrement = dx / steps
+    val yIncrement = dy / steps
+    var x : Double = board.playerEntity.pos._1
+    var y : Double = board.playerEntity.pos._2
+    var finish = false
+    while (!finish) {
+      x += xIncrement
+      y += yIncrement
+      if ( 
+        !board.grid(x.toInt)(y.toInt).explored 
+        || board.grid(x.toInt)(y.toInt).blocking_sight 
+        || (board.distance(board.playerEntity.pos, (x.toInt,y.toInt)) > highlightLength) 
+      ) {
+        finish = true
+      } else {
+        hCells = hCells :+ (x.toInt,y.toInt)
+      }
+    }
+    return hCells
+  }
+
 
 }
