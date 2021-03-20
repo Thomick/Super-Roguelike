81fd790b2c348f1172ac9cba332da6267086463c
Léo Mangel <leomangel@protonmail.com>
2021-03-20 17:28:54 +0100

    Implement the hightlighting when firing

--- src/main/scala/Cursor.scala
+++ src/main/scala/Cursor.scala
@@ -5,10 +5,14 @@ import map_objects._
 
 class Cursor (board : GameBoard) {
   val image = "src/main/resources/cursor.png"
+  val highlightImage = "src/main/resources/highlight.png"
   var xpos = 0
   var ypos = 0
   var visible = false
   var highlightPath = false
+  var highlightLength = 0
+  var areaOfEffect = false
+  var rangeOfEffect = 0
   def pos : (Int,Int) = (xpos,ypos)
   def backToPlayer : Unit = {
     xpos = board.playerEntity.pos._1
@@ -20,6 +24,20 @@ class Cursor (board : GameBoard) {
   def makeInvisible : Unit = {
     visible = false
   }
+  def activateAOE (range : Int) : Unit = {
+    areaOfEffect = true
+    rangeOfEffect = range
+  }
+  def deactivateAOE : Unit = {
+    areaOfEffect = false
+  }
+  def activateHighlight (length : Int ) : Unit = {
+    highlightPath = true
+    highlightLength = length
+  }
+  def deactivateHighlight : Unit = {
+    highlightPath = false
+  }
   def move (dir : Direction.Value): Unit = {
     val newpos = Direction.nextPos((xpos,ypos),dir)
     if (board.inGrid(newpos)) {
@@ -27,4 +45,6 @@ class Cursor (board : GameBoard) {
       ypos = newpos._2
     }
   }
+  def highlightedCells : ... = {
+
 }
