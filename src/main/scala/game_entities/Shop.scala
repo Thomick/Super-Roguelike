253fce406412ea480bf5b7a7d2c937ff7c626db9
Thomas MICHEL <tmdu13@gmail.com>
2021-04-02 16:44:50 +0200

    Add screwdriver

--- src/main/scala/game_entities/Shop.scala
+++ src/main/scala/game_entities/Shop.scala
@@ -35,6 +35,7 @@ class Shopkeeper(init_pos: (Int, Int), b: GameBoard)
   var lastPlayerMet: Option[Player] = None
   forSale += ((new LaserChainsaw, 10), (new IronHelmet, 3), (new Morphin, 1), (new Bandage, 3))
   forSale += ((new HackingTools, 10), (new ElectronicComponents, 5), (new Grenade, 3), (new EMPGrenade, 3))
+  forSale += ((new Screwdriver, 10))
 
   def sell(itemIndex: Int, player: Player): Unit = {
     if (forSale.length > itemIndex && itemIndex >= 0)
--- src/main/scala/items/OtherItems.scala
+++ src/main/scala/items/OtherItems.scala
@@ -76,3 +76,57 @@ class ElectronicComponents extends AbstractItem with Consumable {
     super.consume(character)
   }
 }
+
+class Screwdriver extends AbstractItem with Consumable {
+  var name = "Screwdriver"
+  val description = "Can be used to disassemble equipment and get electronic components"
+  val weight = 100
+
+  val consumptionMessage = "You can choose an item to disassemble"
+
+  // Open a menu that allows to upgrade an item
+  override def consume(character: Character): String = {
+    if (!character.isInstanceOf[Player])
+      return "Only players can dismantle items"
+    val player = character.asInstanceOf[Player]
+    val upgradableItems = new mutable.ArrayBuffer[(Upgradable, Int)]
+    //Gathers all upgradable items from player inventory and equiped inventory
+    player.equipedItems.foreach(item =>
+      if (item.isInstanceOf[Upgradable])
+        upgradableItems += ((item.asInstanceOf[Upgradable], 0))
+    )
+    player.inventory.foreach(item =>
+      if (item.isInstanceOf[Upgradable])
+        upgradableItems += ((item.asInstanceOf[Upgradable], 1))
+    )
+    // Menu used to show upgradable items in inventory
+    val menu = new Menu {
+      override val name = "Disassemble"
+
+      if (upgradableItems.isEmpty)
+        items += (("You do not have any item you can disassemble", ""))
+      else
+        upgradableItems.foreach(item => items += ((item._1.name, "")))
+
+      override def confirm(): Unit = {
+        if (cursorIndex < upgradableItems.size) {
+          player.writeLog("You disassemble " + upgradableItems(cursorIndex)._1.name)
+          if (upgradableItems(cursorIndex)._2 == 0)
+            player.equipedItems -= upgradableItems(cursorIndex)._1
+          else
+            player.inventory -= upgradableItems(cursorIndex)._1
+          player.inventory += new ElectronicComponents
+        }
+        if (UI.menuStack.top == this) {
+          UI.menuStack.pop
+          onClosing()
+        }
+      }
+
+      override def onClosing(): Unit =
+        player.inventory += new Screwdriver
+    }
+    UI.menuStack.push(menu)
+    super.consume(character)
+  }
+}
